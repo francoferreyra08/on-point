1825abae133f91937cd8c31eba53cfad312b0349
Punto Digital <fferreyra998@gmail.com>
2019-12-17 10:32:43 -0300

    agregamos bootstrap

---
+++ principal.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>principal</title>
+  </head>
+  <body>
+   <form class="" action="index.html" method="post">
+     
+   </form>
+
+
+  </body>
+</html>
